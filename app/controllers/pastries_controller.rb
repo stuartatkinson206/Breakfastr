458e8c1c2c5245ae079fb614a319de1c6ccdc01c
Stuart Atkinson <stuartatkinson@Stuarts-MacBook-Pro.local>
2015-03-05 12:22:38 +0000

    loop of pastries

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -1,5 +1,19 @@
 class PastriesController < ApplicationController
   def index
+  	# create array of hashes
+  	# will become seed data for migration
+  	@pastries = [
+  				{
+  					name: "Croissant",
+  					price_in_pence: 350,
+  					description:" Butter one"  		
+				},
+				{
+					name: "Pain au chocolat",
+					price_in_pence: 380,
+					description: "choccy one",
+				}]
+				
   end
 
   def show
