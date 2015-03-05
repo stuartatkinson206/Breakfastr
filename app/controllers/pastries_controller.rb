09fd7516018bf5cf1088f07f002daf56d7ca8d89
Stuart Atkinson <stuartatkinson@Stuarts-MacBook-Pro.local>
2015-03-05 14:51:27 +0000

    RESTful actions

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -13,8 +13,47 @@ class PastriesController < ApplicationController
   end
 
   def new
+  	@pastry = Pastry.new
+  
+  end
+
+  def create
+  	@pastry = Pastry.new(pastry_params)
+  	if @pastry.save
+  		flash[:success] = "Pastry added!"
+  		redirect_to root_path
+  	else
+  		flash[:Error] = "Twat"
+  		render :new	
+  	end
   end
 
   def edit
+    @pastry = Pastry.find(params[:id])
+
   end
+
+  def update
+    @pastry = Pastry.find(params[:id])
+    if @pastry.update(pastry_params)
+      flash[:success] = "Pastry updated"
+      redirect_to pastry_path(@pastry)
+    else 
+      flash[:error] = "Twat"
+      render :edit
+    end
+  end
+
+  def destroy
+    @pastry = Pastry.find(params[:id])
+    @pastry.destroy
+    flash[:success] = "Pastry destroyed!!"
+      redirect_to root_path
+  end
+
+	private
+	def pastry_params
+		params.require(:pastry).permit(:name, :price_in_pence, :description)
+	end
+
 end
